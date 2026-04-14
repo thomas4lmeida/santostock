6baaa56d0f8d2a14ae8170c6945ad218feb9dee1
Thomas Almeida <thomas.k3lvin@gmail.com>
2026-04-13 23:24:24 -0300

    refactor: drop whatsapp_number from users
    The WhatsApp notifications feature is retired in the inventory/warehouse
    pivot. Remove the column migration and scrub the attribute from the
    User model's Fillable declaration.

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Spatie\Permission\Traits\HasRoles;
 
-#[Fillable(['name', 'email', 'password', 'whatsapp_number'])]
+#[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable
 {
